22713956186a65ffd941b539fc3a2b838347a2d5
Gary McGovern <gazzaplays@gmail.com>
2020-02-06 23:06:03 +0000

    Added function to connect to database

---
+++ database.py
@@ -0,0 +1,20 @@
+import pymysql.cursors
+import os
+
+
+def connectToDb():
+    # Get credentials
+    uri = os.environ.get('CLEARDB_DATABASE_URL')
+    host = uri.split('@')[1].split('/')[0]
+    user = uri.split('://')[1].split(':')[0]
+    password = uri.split(':')[2].split('@')[0]
+    dbName = uri.split('/')[3].split('?')[0]
+
+    # Connect to the database
+    connection = pymysql.connect(host=host,
+                                 user=user,
+                                 password=password,
+                                 db=dbName,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
