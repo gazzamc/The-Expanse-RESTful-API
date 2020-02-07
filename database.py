8dcd777f88cdffe181d910626cd2610eeb39949b
Gary McGovern <gazzaplays@gmail.com>
2020-02-07 00:35:24 +0000

    Added check in database.py to see if uri existed

--- database.py
+++ database.py
@@ -5,16 +5,21 @@ import os
 def connectToDb():
     # Get credentials
     uri = os.environ.get('CLEARDB_DATABASE_URL')
-    host = uri.split('@')[1].split('/')[0]
-    user = uri.split('://')[1].split(':')[0]
-    password = uri.split(':')[2].split('@')[0]
-    dbName = uri.split('/')[3].split('?')[0]
 
-    # Connect to the database
-    connection = pymysql.connect(host=host,
-                                 user=user,
-                                 password=password,
-                                 db=dbName,
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
+    if uri is not None:
+        host = uri.split('@')[1].split('/')[0]
+        user = uri.split('://')[1].split(':')[0]
+        password = uri.split(':')[2].split('@')[0]
+        dbName = uri.split('/')[3].split('?')[0]
+
+        # Connect to the database
+        connection = pymysql.connect(host=host,
+                                    user=user,
+                                    password=password,
+                                    db=dbName,
+                                    charset='utf8mb4',
+                                    cursorclass=pymysql.cursors.DictCursor)
+    else:
+        connection = "failed"
+
     return connection
