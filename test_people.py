a297a36c655eecc6848f32e49ef45080c324a034
Gary McGovern <gazzaplays@gmail.com>
2020-02-19 10:21:14 +0000

    Added some basic tests using unittest to check behaviour of GET method when calling /people endpoint

---
+++ test_people.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+from routes import app
+
+
+class PeopleTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_api_people_valid_record(self):
+        rv = self.app.get('/api/people/1')
+        self.get_json = json.loads(rv.data)
+        self.assertEqual(self.get_json['data']['name'], 'Captain McDowell')
+
+    def test_api_people_invalid_record(self):
+        rv = self.app.get('/api/people/2')
+        self.get_json = json.loads(rv.data)
+        self.assertEqual(self.get_json['message'], 'Record does not exist')
+
+    def test_api_people_invalid_id(self):
+        rv = self.app.get('/api/people/james')
+        self.get_json = json.loads(rv.data)
+        self.assertEqual(self.get_json['message'], 'Bad Request. ID must be an integer')
+
+
+if __name__ == '__main__':
+    unittest.main()
